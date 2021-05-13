40bd28b83e489c7f77a4adbbac1f590cb012b2c4
resolt <pdmmichaelsen@gmail.com>
2021-05-13 12:25:35 +0200

    -readded simple os import

--- dlunpack/main.py
+++ dlunpack/main.py
@@ -1,4 +1,4 @@
-from os.path import join, exists, splitext, isdir, walk
+import os
 import sys
 import shutil
 import pathlib
@@ -17,13 +17,13 @@ def main():
 
 	path_7z = get_7z()
 
-	for root, _, filenames in walk(args.dir):
-		rarfiles = filter(lambda x: splitext(x)[1] == '.rar', filenames)
+	for root, _, filenames in os.walk(args.dir):
+		rarfiles = filter(lambda x: os.path.splitext(x)[1] == '.rar', filenames)
 		for filename in rarfiles:
-			filepath = join(root, filename)
-			flagpath = splitext(filepath)[0] + '.isunpacked'
-			couchpath = splitext(filepath)[0] + '.extracted.ignore'
-			if exists(flagpath) or exists(couchpath):
+			filepath = os.path.join(root, filename)
+			flagpath = os.path.splitext(filepath)[0] + '.isunpacked'
+			couchpath = os.path.splitext(filepath)[0] + '.extracted.ignore'
+			if os.path.exists(flagpath) or os.path.exists(couchpath):
 				continue
 			age = get_age(filepath)
 			if age < REQUIRED_AGE:
@@ -65,10 +65,10 @@ def get_args():
 	if args.dir is None:
 		argparser.error('--dir must be provided')
 
-	if not exists(args.dir):
+	if not os.path.exists(args.dir):
 		argparser.error(f'Target directory does not exists : {args.dir}')
 
-	if not isdir(args.dir):
+	if not os.path.isdir(args.dir):
 		argparser.error(f'Target is not a directory : {args.dir}')
 
 	return args
